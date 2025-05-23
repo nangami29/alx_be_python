081ace5c0aaccb52a84915823687f466d53152e9
Nangami Rachael-Loureen <rachaelnangami29@gmail.com>
2025-05-23 13:18:50 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
-principal = 1000,
-rate = 0.05,
-time = 3, 
+principal = 1000
+rate = 0.05
+time = 3
 
-interest = principal * rate * time,
+interest = principal * rate * time
 
 print('The simple interest is: ', interest)
