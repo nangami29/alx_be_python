5502f162dc5d96b19cf6d2f3e5cd989c5996e0e3
Nangami Rachael-Loureen <rachaelnangami29@gmail.com>
2025-05-23 13:18:04 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,11 +1,11 @@
-number1 = 10,
-number2 = 5,
+number1 = 10
+number2 = 5
 
-addition = number1 + number2,
+addition = number1 + number2
 print("Addition of ", number1, 'and', number2,'is', addition )
 
-difference = number2 - number1,
+difference = number2 - number1
 print("Difference of ", number1, 'and', number2, 'is', difference)
 
-product = number1 * number2,
+product = number1 * number2
 print("Product of ", number1, 'and', number2, 'is', product)
