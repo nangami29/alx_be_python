dbe794a0e7519fb1d67b3cc995f73ae4ecc087d6
nangami29 <rachaelnangami29@gmail.com>
2025-06-02 18:54:33 +0300

    arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+def perform_operation(num1, num2, operation=('add', 'subtract', 'multiply', 'divide')):
+    match operation:
+        case 'add':
+            return num1+ num2
+        case 'subtract':
+            return num1 -num2
+        
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if num2 == 0:
+                return "Error, cannot divide by zero"
+            else:
+                return num1/num2
+        
+
+
+
+
+
