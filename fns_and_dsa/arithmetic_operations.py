99d85d16daef9f02f41dbd290459940b8e90bb71
nangami29 <rachaelnangami29@gmail.com>
2025-06-02 19:03:10 +0300

    changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,7 +10,7 @@ def perform_operation(num1, num2, operation):
         case 'divide':
             if num2 == 0:
                 return "Error, cannot divide by zero"
-            else:
+            elif num2 > 0 :
                 return num1/num2
         
 
