d5e736c383d66cd5e5d39e30fea6bc6852fe15b4
nangami29 <rachaelnangami29@gmail.com>
2025-06-06 16:42:56 +0300

    temprature

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,24 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR=(5/9)
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
+
+def convert_to_celsius(fahrenheit):
+    temperature=fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
+    print(f"temperature converted to Celsius: {temperature}")
+    return temperature
+
+def convert_to_fahrenheit(celsius):
+    temperature_converted_Fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
+    print(f" temperature converted to Fahrenheit: { temperature_converted_Fahrenheit}")
+    return  temperature_converted_Fahrenheit
+
+temp = float(input("Enter the temperature to convert: "))
+specific_temp=input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
+match specific_temp:
+    case 'C':
+        print(f"{temp} is {convert_to_fahrenheit}")
+    case 'F':
+        print(f"{temp} is {convert_to_celsius}")
+    case _ :
+        print('Invalid temperature. Please enter a numeric value.')
+
