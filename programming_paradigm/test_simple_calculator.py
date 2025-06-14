7cbb61353f280f44fac30e09194ca50c31027fb1
nangami29 <rachaelnangami29@gmail.com>
2025-06-14 15:04:02 +0300

    calculator

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,40 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+         """Set up the SimpleCalculator instance before each test."""
+         self.calc= SimpleCalculator()
+    
+        
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(5, 7), 12)
+    
+    def subtract(self):
+        """Test the subtraction method."""
+        self.assertEqual(self.calc.subtract(5, 7), -2)
+        self.assertEqual(self.calc.subtract(-5, 7), 12)
+        self.assertEqual(self.calc.subtract(10, 7), -2)
+    def multiply(self):
+        """Test the multiplication method."""
+        self.assertEqual(self.calc.multiply(5 * 7), 35)
+        self.assertEqual(self.calc.multiply(-5 * 7), -35)
+        self.assertEqual(self.calc.multiply(-5 * -7), 35)
+
+
+    def divide(self):
+        """Test the division method."""
+        
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(12, 5), 2.4)
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(5, 0)
+
+if __name__ == "__main__":
+   TestSimpleCalculator()
+
+
