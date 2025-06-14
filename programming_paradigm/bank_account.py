e60b9f407a4e3a8b8b4871396024675ac9385c12
nangami29 <rachaelnangami29@gmail.com>
2025-06-14 12:29:19 +0300

    changes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,19 +1,19 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
         self.account_balance=initial_balance
-        # Encapsulation and behaviours
-        def deposit (self, amount):
-            self.amount += amount
-        def withdraw (self, amount):
-            self.amount = amount
-            if amount <= self.account_balance: 
-              self.amount -= amount
-              return  True, 
-           
-                
-            else :
-                return False   
-
-        def display_balance (self):
-            print('Current Balance: {self.account_balance}')
+    # Encapsulation and behaviours
+    def deposit (self, amount):
+        self.amount += amount
+    def withdraw (self, amount):
+        self.amount = amount
+        if amount <= self.account_balance: 
+            self.amount -= amount
+            return  True, 
+        
             
+        else :
+            return False   
+
+    def display_balance (self):
+        print('Current Balance: {self.account_balance}')
+        
