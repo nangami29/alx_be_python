725de7f38de149606ee72e3693058ed58c9bce7a
nangami29 <rachaelnangami29@gmail.com>
2025-06-19 17:36:48 +0300

    library

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,15 +2,28 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+
+    def __str__(self):
+        return f"{self.title}, {self.author}"
+
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size= file_size
 
+    def __str__(self):
+        return f"{self.title}, {self.author}, {self.file_size}"
+
+
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count= page_count
+
+    def __str__(self):
+        return f"{self.title}, {self.author}, {self.page_count}"
+
+
 class Library:
     def add_book(self, book):
         return book.append(Book, EBook, PrintBook)
